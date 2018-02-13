1596632df5422c7304da47c8851181e1cc1e3506
Guillaume Lenoir <guillaume.lenoir@gleo.fr>
2018-02-13 13:18:45 +0100

    update

--- Training-hook/src/main/java/fr/smile/training/services/ExtUserLocalService.java
+++ Training-hook/src/main/java/fr/smile/training/services/ExtUserLocalService.java
@@ -2,24 +2,28 @@ package fr.smile.training.services;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.User;
-import com.liferay.portal.service.UserLocalServiceWrapper;
 import com.liferay.portal.service.UserLocalService;
+import com.liferay.portal.service.UserLocalServiceWrapper;
 
 public class ExtUserLocalService extends UserLocalServiceWrapper {
-	/* (non-Java-doc)
-	 * @see com.liferay.portal.service.UserLocalServiceWrapper#UserLocalServiceWrapper(UserLocalService userLocalService)
-	 */
+	
 	public ExtUserLocalService(UserLocalService userLocalService) {
 		super(userLocalService);
 	}
 
+	/**
+     * The Logger
+     */
+    private static Log LOGGER = LogFactoryUtil.getLog(ExtUserLocalService.class);
+
 	@Override
 	public User deleteUser(long userId) throws PortalException, SystemException {
-		// TODO Auto-generated method stub
 		User user = getUser(userId);
 		
-		System.out.println(user);
+		LOGGER.info("user =" + user);
 		
 		return super.deleteUser(userId);
 	}
